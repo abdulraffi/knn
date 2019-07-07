40276c33f9b97dd77cd65d59b411e0bb94f2c034
Roji <rojjiellndi@gmail.com>
2019-07-07 22:53:33 +0700

    roji

--- app/Api/V1/Controllers/KNNController.php
+++ app/Api/V1/Controllers/KNNController.php
@@ -6,6 +6,7 @@
  * Time: 1:52
  */
 
+
 namespace App\Api\V1\Controllers;
 
 
@@ -22,8 +23,7 @@ class KNNController extends Controller
 {
     public function knn(){
 
-        $classifier = new KNearestNeighbors(7);
-//        $logistic = new LogisticRegression();
+        $classifier = new KNearestNeighbors(3);
 
         $user = Data::get();
 
@@ -34,14 +34,14 @@ class KNNController extends Controller
             $data = [
                 $item->total_follower,
                 $item->total_following,
-                $item->total_media_url,
-                $item->total_url,
-                $item->total_mention,
-                $item->total_RT,
-                $item->total_hashtag,
-                $item->total_huruf_besar,
-                $item->total_tanda_baca,
-                $item->total_emoji,
+                // $item->total_media_url,
+                // $item->total_url,
+                // $item->total_mention,
+                // $item->total_RT,
+                // $item->total_hashtag,
+                // $item->total_huruf_besar,
+                // $item->total_tanda_baca,
+                // $item->total_emoji,
                 $item->total_kata,
                 $item->rata2_kata,
                 $item->total_karakter,
@@ -52,22 +52,7 @@ class KNNController extends Controller
             $datatest[] = $data;
             $labeltest[] = $label;
         }
-//
-//        $test1 = [1000, 200, 100000];
-//        $test2 = [1500, 250, 108000];
-//        $test3 = [1070, 203, 102000];
-//
-//        $label1 = 'e';
-//        $label2 = 'a';
-//        $label3 = 'c';
-//
-//        $labeltest[] = $label1;
-//        $labeltest[] = $label2;
-//        $labeltest[] = $label3;
-//
-//        $datatest[] = $test1;
-//        $datatest[] = $test2;
-//        $datatest[] = $test3;
+
 
         $classifier->train($datatest, $labeltest);
         $datauji = DataLatih::get();
@@ -78,14 +63,14 @@ class KNNController extends Controller
             $data = [
                 $item->total_follower,
                 $item->total_following,
-                $item->total_media_url,
-                $item->total_url,
-                $item->total_mention,
-                $item->total_RT,
-                $item->total_hashtag,
-                $item->total_huruf_besar,
-                $item->total_tanda_baca,
-                $item->total_emoji,
+                // $item->total_media_url,
+                // $item->total_url,
+                // $item->total_mention,
+                // $item->total_RT,
+                // $item->total_hashtag,
+                // $item->total_huruf_besar,
+                // $item->total_tanda_baca,
+                // $item->total_emoji,
                 $item->total_kata,
                 $item->rata2_kata,
                 $item->total_karakter,
