f95fd796308ddbfb2e1d3d1866aa2b94539348f9
fadhilghifari <fadhilghifari.fg@gmail.com>
2019-07-07 15:57:31 +0700

    ui

---
+++ resources/views/form/form.blade.php
@@ -0,0 +1,140 @@
+<html>
+<head> 
+<title> From </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+        <div class="container" style="margin-top:2%; margin-bottom: 2%;">
+                <div class="card">
+                        <div class="card-header">Form</div>
+                            <div class="card-body">
+                                    <div class="row">
+                                    
+                                            <div class="col">
+                                                <button class="btn btn-success btn-sm btn-block">Button 1</button>    
+                                            </div>
+                                            
+                                            <div class="col">
+                                                <button class="btn btn-danger btn-sm btn-block">Button 2</button>
+                                            </div>
+                                                    
+                                        </div>
+                                <div class="row">
+                                    
+                                    <div class="col">
+                                          
+                                            <div class="form-check form-check-inline">
+                                                    <div class="form-group form-check">
+                                                            <input type="checkbox" class="form-check-input" id="total_follower">
+                                                            <label class="form-check-label" for="total_follower" style="margin-top: 4%;">Total Follower</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_following">
+                                                            <label class="form-check-label" for="total_following" style="margin-top: 4%;">Total Following</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_media_url">
+                                                            <label class="form-check-label" for="total_media_url" style="margin-top: 4%;">Total Media URL</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_url">
+                                                            <label class="form-check-label" for="total_url" style="margin-top: 4%;">Total URL</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_mention">
+                                                            <label class="form-check-label" for="total_mention" style="margin-top: 4%;">Total Mention</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_RT">
+                                                            <label class="form-check-label" for="total_RT" style="margin-top: 4%;">Total RT</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_hashtag">
+                                                            <label class="form-check-label" for="total_hashtag" style="margin-top: 4%;">Total Hastag</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_huruf_besar">
+                                                            <label class="form-check-label" for="total_huruf_besar" style="margin-top: 4%;">Total Huruf Besar</label>
+                                                            <br>
+                                                            <input type="checkbox" class="form-check-input" id="total_follower">
+                                                            <label class="form-check-label" for="total_follower" style="margin-top: 4%;">Total Follower</label>
+                                                            
+                                                    </div>
+                                              </div>
+                                            
+                                              
+                                    </div>
+                                    
+                                    <div class="col">
+                                            <div class="form-check form-check-inline">
+                                                <div class="form-group form-check">
+                                                    <input type="checkbox" class="form-check-input" id="total_huruf_besar">
+                                                    <label class="form-check-label" for="total_huruf_besar" style="margin-top: 4%;">Total Huruf Besar</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="total_tanda_baca">
+                                                    <label class="form-check-label" for="total_tanda_baca" style="margin-top: 4%;">Total Tanda Baca</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="total_emoji">
+                                                    <label class="form-check-label" for="total_emoji" style="margin-top: 4%;">Total Emoji</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="total_kata">
+                                                    <label class="form-check-label" for="total_kata" style="margin-top: 4%;">Total Kata</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="rata2_kata">
+                                                    <label class="form-check-label" for="rata2_kata" style="margin-top: 4%;">Rata - rata Kata</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="total_karakter">
+                                                    <label class="form-check-label" for="total_karakter" style="margin-top: 4%;">Total Karakter</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="rata2_karakter">
+                                                    <label class="form-check-label" for="rata2_karakter" style="margin-top: 4%;">Rata - rata Karakter</label>
+                                                    <br>
+                                                    <input type="checkbox" class="form-check-input" id="TF_IDF">
+                                                    <label class="form-check-label" for="TF_IDF" style="margin-top: 4%;">TF IDF</label>
+                                                    <br>
+                                                    <button class="btn btn-primary" style="margin-top: 5%;">Button 3</button>
+
+
+                                                </div>
+                                            </div>
+                                    </div>
+                                            
+                                </div>
+                                <table class="table">
+                                      <table class="table">
+                                        <thead class="thead-light">
+                                          <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">First</th>
+                                            <th scope="col">Last</th>
+                                            <th scope="col">Handle</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                          </tr>
+                                          <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                          </tr>
+                                          <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                            </div>
+                        </div>
+                </div>
+        </div>
+
+
+</body>
+
+</html>
